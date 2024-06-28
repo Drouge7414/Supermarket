e130f88405d65c6559d5a6b7ccf6302c6197fe5c
Drouge7414 <108477680+Drouge7414@users.noreply.github.com>
2024-06-28 16:57:38 -0400

    Create Supermarket.cpp

---
+++ Supermarket.cpp
@@ -0,0 +1,80 @@
+// SuperMarket.cpp - This program creates a report that lists weekly hours worked 
+// by employees of a supermarket. The report lists total hours for 
+// each day of one week. 
+// Input:  Interactive
+// Output: Report. 
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main() 
+{
+   // Declare variables.
+   const string HEAD1 = "WEEKLY HOURS WORKED";
+   const string DAY_FOOTER = "                              Previous Day Total ";
+   // Leading spaces in DAY_FOOTER are intentional.
+   const string SENTINEL = "done";  // Named constant for sentinel value. 
+   double hoursWorked = 0;  // Current record hours.
+   string dayOfWeek;  // Current record day of week.
+   double hoursTotal = 0;  // Hours total for a day.
+   string prevDay = "";  // Previous day of week.
+   bool notDone = true;  // loop control
+  
+   // Print two blank lines.
+   cout << endl << endl; 
+   // Print heading.
+   cout << "\t\t\t\t\t" << HEAD1 << endl;
+   // Print two blank lines.
+   cout << endl << endl;  
+
+   // Read first record 
+   cout << "Enter day of week or done to quit: ";
+   cin >> dayOfWeek;
+ 
+   if(dayOfWeek  == SENTINEL)
+      notDone = false;
+   else
+   {
+      cout << "Enter hours worked: ";
+      cin >> hoursTotal;
+      prevDay = dayOfWeek;
+      hoursWorked = hoursWorked + hoursTotal;
+      cout << dayOfWeek <<": " << hoursTotal << endl;
+   }	   
+		
+   
+   
+  
+      // Implement control break logic here
+      // Include work done in the dayChange() function
+
+
+    while(notDone)
+   {	
+   cout << "Enter day of week or done to quit: ";
+   cin >> dayOfWeek;
+      
+   {
+      if (dayOfWeek == prevDay)
+   
+   
+
+      cout << "Enter hours worked: ";
+      cin >> hoursTotal;
+      prevDay = dayOfWeek;
+      hoursTotal = hoursTotal + hoursTotal;
+      hoursWorked = hoursWorked + hoursTotal;
+
+ 
+
+
+   cout << "\t\t" << DAY_FOOTER << hoursTotal << endl;
+   cout << dayOfWeek << ": " << hoursTotal<< endl;
+   }}
+
+
+   if (hoursTotal > 0)
+      cout << "\t\t" << DAY_FOOTER << hoursTotal << endl;			
+   return 0;
+
+}
